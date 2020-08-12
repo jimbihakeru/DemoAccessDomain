2db56ed7e960fa7a6834d95b75a9f46e2f917a6a
Thuan Nguyen <thuannh1511@gmail.com>
2020-08-12 16:11:43 +0700

    commit jimbi

--- src/test/java/testing/API/GetRequestAutomation.java
+++ src/test/java/testing/API/GetRequestAutomation.java
@@ -144,7 +144,7 @@ public class GetRequestAutomation {
 
 				String url = prop.getProperty("url" + i);
 				driver.get(url);
-
+				Thread.sleep(3000);
 				test = extent.createTest(url);
 				if (driver.findElements(By.xpath(IDElement)).size() != 0) {
 					test.log(Status.PASS, url + "----- Access OK");
